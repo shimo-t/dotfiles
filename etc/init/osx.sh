3e2ac756961a07682ff48a9569722aef1bfd2b01
Takahisa Shimoda <shimo.t.122@gmail.com>
2017-05-21 04:57:49 +0900

    Add installation of fish and fisherman for OSX

--- etc/init/osx.sh
+++ etc/init/osx.sh
@@ -16,6 +16,11 @@ brew install git
 brew install tmux
 brew install ffmpeg
 
+brew install fish
+
+curl -Lo $HOME/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+echo "fisher z" | fish
+
 # pyenv
 if [ ! -d ~/.pyenv ]; then
     git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
