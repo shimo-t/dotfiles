2413b0a600a8ad1117c48377000391aac9dbf06c
Takahisa Shimoda <shimo.t.122@gmail.com>
2018-04-26 22:26:18 +0900

    Update scope option

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,19 +1,19 @@
-set -x PATH $HOME/.local/bin $PATH
+set -gx PATH $HOME/.local/bin $PATH
 
-set -x LANG en_US.UTF-8
-set -x LC_CTYPE en_US.UTF-8
-set -x LC_NUMERIC en_US.UTF-8
-set -x LC_TIME en_US.UTF-8
-set -x LC_COLLATE en_US.UTF-8
-set -x LC_MONETARY en_US.UTF-8
-set -x LC_MESSAGES en_US.UTF-8
+set -gx LANG en_US.UTF-8
+set -gx LC_CTYPE en_US.UTF-8
+set -gx LC_NUMERIC en_US.UTF-8
+set -gx LC_TIME en_US.UTF-8
+set -gx LC_COLLATE en_US.UTF-8
+set -gx LC_MONETARY en_US.UTF-8
+set -gx LC_MESSAGES en_US.UTF-8
 
 # for python
-set -x PYENV_ROOT $HOME/.pyenv
-set -x PATH $PYENV_ROOT/bin $PATH
+set -gx PYENV_ROOT $HOME/.pyenv
+set -gx PATH $PYENV_ROOT/bin $PATH
 . (pyenv init - | psub)
 
 # for plenv
-set -x PLENV_ROOT $HOME/.plenv
-set -x PATH $PLENV_ROOT/bin $PATH
+set -gx PLENV_ROOT $HOME/.plenv
+set -gx PATH $PLENV_ROOT/bin $PATH
 . (plenv init - | psub)
