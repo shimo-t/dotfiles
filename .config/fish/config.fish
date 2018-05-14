4f015e18663ab0875a655c1b13e20aa403346c75
Takahisa Shimoda <shimo.t.122@gmail.com>
2018-05-15 01:12:27 +0900

    Add key_bindings.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,4 @@ set fish_greeting
 
 . $HOME/.config/fish/env.fish
 . $HOME/.config/fish/aliases.fish
-
-function fish_user_key_bindings
-end
+. $HOME/.config/fish/key_bindings.fish
---
+++ .config/fish/key_bindings.fish
@@ -0,0 +1,7 @@
+# fishのキーバインド一覧
+# https://qiita.com/ryotako/items/ba7483b6fde025c91cef
+
+function fish_user_key_bindings
+    bind \co '__fzf_find_file'
+    bind \cr '__fzf_reverse_isearch'
+end
