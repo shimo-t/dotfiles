cab920ed7a538f02f735f408cb3b266aa67f4455
Takahisa Shimoda <shimo.t.122@gmail.com>
2018-06-26 01:22:25 +0900

    Add fish theme switcher

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -19,3 +19,36 @@ function zf
     z -l | fzf +s | awk '{print $2}' | read -l recent; and cd $recent
   end
 end
+
+function __just_a_dollar
+  function fish_prompt
+    echo -n '$ '
+  end
+end
+
+function __agnoster
+  function fish_prompt
+    set -g RETVAL $status
+    prompt_status
+    prompt_virtual_env
+    prompt_user
+    prompt_dir
+    type -q hg;  and prompt_hg
+    type -q git; and prompt_git
+    type -q svn; and prompt_svn
+    prompt_finish
+  end
+end
+
+function theme -d "Switch fish theme."
+  switch $argv[1]
+    case jad
+      __just_a_dollar
+    case agnoster
+      __agnoster
+    case default
+      __agnoster
+    case '*'
+      echo "$argv[1] doesn't exist."
+  end
+end
