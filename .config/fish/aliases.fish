99a39d051849542d75fcae6d0c73ca2b947f8b7c
Takahisa Shimoda <shimo.t.122@gmail.com>
2023-12-24 22:16:19 +0900

    Remove a command to change fish theme

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -20,40 +20,6 @@ function zf
   end
 end
 
-function __just_a_dollar
-  function fish_prompt
-    echo -n '$ '
-  end
-end
-
-# TODO: DONOT call directly agnoster function
-function __agnoster
-  function fish_prompt
-    set -g RETVAL $status
-    prompt_status
-    prompt_virtual_env
-    prompt_user
-    prompt_dir
-    type -q hg;  and prompt_hg
-    type -q git; and prompt_git
-    type -q svn; and prompt_svn
-    prompt_finish
-  end
-end
-
-function theme -d "Switch fish theme."
-  switch $argv[1]
-    case jad
-      __just_a_dollar
-    case agnoster
-      __agnoster
-    case default
-      __agnoster
-    case '*'
-      echo "$argv[1] doesn't exist."
-  end
-end
-
 function gitco -d "Git checkout with fzf"
   if count $argv > /dev/null
     git checkout (git branch | fzf -1 +s -q $argv[1] | tr -d ' ' | tr -d '*')
