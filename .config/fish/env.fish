2f439b5cb5d0575286e1dbc2233e99f1495ec758
Takahisa Shimoda <shimo.t.122@gmail.com>
2023-12-24 22:10:35 +0900

    Update fish theme color scheme dracula

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -8,6 +8,8 @@ set -gx LC_COLLATE en_US.UTF-8
 set -gx LC_MONETARY en_US.UTF-8
 set -gx LC_MESSAGES en_US.UTF-8
 
+set -g theme_color_scheme dracula
+
 # for fzf
 set -gx FZF_DEFAULT_OPTS '--height 50% --reverse'
 set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
